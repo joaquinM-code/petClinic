b08b2a54e07223e8f8b0703aa69bf43daaf7e55f
ecatom <netpassrecovers@hotmail.com>
2020-04-06 17:51:31 +0200

    refactoring to multimodule project. closes #1

---
+++ pet-clinic-web/src/main/java/com/ecatom/sfpetclinic/SfPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ecatom.sfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfPetClinicApplication.class, args);
+	}
+
+}
---
+++ pet-clinic-web/src/test/java/sfpetclinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
