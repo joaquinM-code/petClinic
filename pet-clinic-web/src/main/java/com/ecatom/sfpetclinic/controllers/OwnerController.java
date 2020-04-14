26577e1c3c393de1c68a6b32bb6254891bc0b4c9
ecatom <netpassrecovers@hotmail.com>
2020-04-14 09:22:58 +0200

    Added owner list . Closes #16

--- pet-clinic-web/src/main/java/com/ecatom/sfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ecatom/sfpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.ecatom.sfpetclinic.controllers;
 
+import com.ecatom.sfpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //Little trick to map the whole class to /owners
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    //Constructor injection from SF5 don't need to call @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/" , "" , "/index" , "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners" , ownerService.findAll());
         return "owners/index";
     }
 }
