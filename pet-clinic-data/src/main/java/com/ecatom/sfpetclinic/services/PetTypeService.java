a7982cd60a147ced65c95d14f03400154ef790f9
ecatom <netpassrecovers@hotmail.com>
2020-04-16 09:17:34 +0200

    Created PetServiceMap Impl. Closes #26

---
+++ pet-clinic-data/src/main/java/com/ecatom/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.ecatom.sfpetclinic.services;
+
+import com.ecatom.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/ecatom/sfpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package com.ecatom.sfpetclinic.services.map;
+
+import com.ecatom.sfpetclinic.model.PetType;
+import com.ecatom.sfpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
