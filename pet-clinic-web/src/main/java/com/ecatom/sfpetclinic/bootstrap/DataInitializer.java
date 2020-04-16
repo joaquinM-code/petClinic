7423a2142b81f9dadb5a974f2c13825e9917a90f
ecatom <netpassrecovers@hotmail.com>
2020-04-16 09:29:53 +0200

    Added PetType to DataInitializer. Closes #29

--- pet-clinic-web/src/main/java/com/ecatom/sfpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/ecatom/sfpetclinic/bootstrap/DataInitializer.java
@@ -1,8 +1,10 @@
 package com.ecatom.sfpetclinic.bootstrap;
 
 import com.ecatom.sfpetclinic.model.Owner;
+import com.ecatom.sfpetclinic.model.PetType;
 import com.ecatom.sfpetclinic.model.Vet;
 import com.ecatom.sfpetclinic.services.OwnerService;
+import com.ecatom.sfpetclinic.services.PetTypeService;
 import com.ecatom.sfpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,18 +14,35 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     //Spring IoC Container will implement the methods
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+
+
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
-
 
     //When the app is completely up everything inside this method will run
     @Override
     public void run(String... args) throws Exception {
 
+        //PetTypes
+        //Results are saved in a variable to be reused latter
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded pet types...........");
+
+
+        //Owners
         Owner owner1 = new Owner();
         owner1.setFirstName("Mike");
         owner1.setLastName("Weston");
@@ -38,6 +57,7 @@ public class DataInitializer implements CommandLineRunner {
 
         System.out.println("Loaded owners.........");
 
+        //Vets
         Vet vet1 = new Vet();
         vet1.setFirstName("John");
         vet1.setLastName("Charleston");
