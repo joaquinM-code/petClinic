005884b869d3fc729bfdd80394b370cc27867824
ecatom <netpassrecovers@hotmail.com>
2020-04-21 11:38:36 +0200

    Created visit entity closes #35

--- pet-clinic-data/src/main/java/com/ecatom/sfpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ecatom/sfpetclinic/model/Pet.java
@@ -1,10 +1,9 @@
 package com.ecatom.sfpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -19,6 +18,9 @@ public class Pet extends BaseEntity{
     @JoinColumn(name = "owner_id")
     private Owner owner;
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL , mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -50,5 +52,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
